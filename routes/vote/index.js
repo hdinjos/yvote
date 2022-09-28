7e58abd63f59586f7b2c0a7dee07caaf964c6cee
hdinjos <hdinjos@gmail.com>
2022-09-28 18:43:29 +0700

    fix add vote

--- routes/vote/index.js
+++ routes/vote/index.js
@@ -29,19 +29,30 @@ router.get("/votes", isOriganizer, async (req, res) => {
 
 router.post("/votes", isChoice, isVote, async (req, res) => {
   const { candidate_id } = req.body;
-  const { id } = req.user;
+  const { id, major_id } = req.user;
 
   if (candidate_id) {
     try {
-      await query("INSERT INTO votes(candidate_id) VALUES(?)", [candidate_id]);
-      await query("UPDATE users SET is_vote=? WHERE id=?", [1, id]);
-      return res.status(201).json({ msg: "Vote is success" });
+      const user = await query(
+        "SELECT major_id FROM candidates as A INNER JOIN users as B ON A.user_id=B.id WHERE A.id=?",
+        [candidate_id]
+      );
+      if (user.length > 0 && user[0].major_id === major_id) {
+        await query("INSERT INTO votes(candidate_id) VALUES(?)", [
+          candidate_id,
+        ]);
+        await query("UPDATE users SET is_vote=? WHERE id=?", [1, id]);
+        return res.status(201).json({ msg: "Vote is success" });
+      } else {
+        return res.status(401).json({ msg: "Candidate is not in your major" });
+      }
     } catch (err) {
       if (err?.code === "ER_NO_REFERENCED_ROW_2") {
         return res
           .status(400)
           .json({ msg: "Candidate is not registered, use another" });
       } else {
+        console.log(err);
         return res.status(403).json({ msg: "Something wrong" });
       }
     }
